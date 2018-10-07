bfb7fbfc2278dd1e31eedb8308a6167b6827e77b
Zmud <zmudmac@gmail.com>
2018-10-07 15:01:49 +0000

    hello world

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: 
-  
+   protect_from_forgery with: :exception
+
   def hello
-    render html: 'hello, world!'
+    render html: "hello, world!"
   end
     
 end
