cbeb02a15fcf7c259f91632c51f4f0aa64acc951
Zmud <zmudmac@gmail.com>
2018-12-13 11:26:49 +0000

    Advanced login with remember me

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -62,5 +62,8 @@ test "password should have a minimum length" do
     assert @user.invalid?
   end
   
+   test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
   
 end
