7a3fa4fee9fb7f1eaf88d33a562e534fd9d21b83
Zmud <zmudmac@gmail.com>
2019-03-03 14:32:53 +0000

    Fixed2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-   config.action_mailer.raise_delfivery_errors = false
+   config.action_mailer.raise_delivery_errors = false
    config.action_mailer.delivery_method = :smtp
    host = 'https://infinite-refuge-96209.herokuapp.com'
    config.action_mailer.default_url_options = { host: host }
